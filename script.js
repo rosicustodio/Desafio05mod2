849989af30198844fd9079cf61367d94889989f4
rosicustodio <rosimarcustodio@gmail.com>
2021-05-27 03:01:26 -0300

    desafio 05

---
+++ script.js
@@ -0,0 +1,16 @@
+let utensilios=["garfo","faca","prato","copo","panela"];
+
+utensilios.unshift("caneca")
+
+utensilios.pop()
+
+utensilios.push("garrafa","coador")
+
+utensilios.shift()
+
+console.log(utensilios)
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+
+    console.log(numbers)
